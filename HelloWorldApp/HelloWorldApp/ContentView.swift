f5b2afbbadcc071874810d1c5c8d69737b17cb02
Yeldar <kaisabekov.yeldar@outlook.com>
2022-08-27 13:53:51 +0600

    Colorized Hello, World and tried to use VSTack

--- HelloWorldApp/HelloWorldApp/ContentView.swift
+++ HelloWorldApp/HelloWorldApp/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            Text("Hello, world!")
+                .foregroundColor(Color.red)
+            Text("Hello, world!")
+                .foregroundColor(Color.green)
+            Text("Hello, world!")
+                .foregroundColor(Color.blue)
+        }
+        
     }
 }
 
