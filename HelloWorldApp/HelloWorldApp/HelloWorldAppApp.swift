9a6d00bc0017fcbd551b1334a2aa847d7e14f1bd
Yeldar <kaisabekov.yeldar@outlook.com>
2022-08-27 13:28:29 +0600

    First app that shows "Hello, World"

---
+++ HelloWorldApp/HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Yeldar Kaisabekov on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
